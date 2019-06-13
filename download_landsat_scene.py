bf848ba4ac9eb4896c47bdb2329d676a658266da
Nicolas Ekicier <nicolas.ekicier@sirs-fr.com>
2019-06-13 09:14:44 +0200

    [EVOL] Changes in function with proxy

--- download_landsat_scene.py
+++ download_landsat_scene.py
@@ -27,7 +27,7 @@ def connect_earthexplorer_proxy(proxy_info,usgs):
     print ("Establishing connection to Earthexplorer with proxy...")
     # contruction d'un "opener" qui utilise une connexion proxy avec autorisation
     cookies = urllib.request.HTTPCookieProcessor()
-    proxy_support = urllib.reProxyHandler({"http" : "http://%(user)s:%(pass)s@%(host)s:%(port)s" % proxy_info,
+    proxy_support = urllib.request.ProxyHandler({"http" : "http://%(user)s:%(pass)s@%(host)s:%(port)s" % proxy_info,
                                           "https" : "http://%(user)s:%(pass)s@%(host)s:%(port)s" % proxy_info})
     opener = urllib.request.build_opener(proxy_support, cookies)
  
@@ -35,7 +35,7 @@ def connect_earthexplorer_proxy(proxy_info,usgs):
     urllib.request.install_opener(opener)
     # deal with csrftoken required by USGS as of 7-20-2016
     data=urllib.request.urlopen("https://ers.cr.usgs.gov").read()
-    m = re.search(r'<input .*?name="csrf_token".*?value="(.*?)"', data)
+    m = re.search(r'<input .*?name="csrf_token".*?value="(.*?)"', data.decode('utf-8'))
     if m:
         token = m.group(1)
     else :
